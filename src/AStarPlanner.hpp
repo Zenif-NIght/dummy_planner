e18ef877153ad1e350a7f8a145578513c863c6ed
ZenifNight <christophertaylorallred@gmail.com>
2020-12-15 10:58:51 -0700

    Changed A* to use an id index into the closed_list.

--- src/AStarPlanner.hpp
+++ src/AStarPlanner.hpp
@@ -9,22 +9,26 @@ private:
     class M_Node
     {
     private:
-        M_Node *m_parent = nullptr;
+        int m_parent;
         Vector2i m_position;
         int m_g;
         double m_h;
         double m_f;
+        int m_id;
     public:
-        M_Node* parent(){return m_parent;};
+        int parent(){return m_parent;};
+        bool has_parent() { return m_parent >= 0; }
         const Vector2i position(){return m_position;};
         const int g(){return m_g;};
         const double h(){return m_h;};
         const double f(){return m_f;};
+        const int id() { return m_id;}
         void set_g(int v) { m_g = v; }
         void set_h(double v) { m_h = v; }
         void set_f(double v) { m_f = v; }
-        M_Node() : m_parent(nullptr),m_g(0),m_h(0),m_f(0) {}
-        M_Node(M_Node *parent, Vector2i position)
+        void set_id(int v) { m_id = v; };
+        M_Node() : m_parent(-1),m_g(0),m_h(0),m_f(0) {}
+        M_Node(Vector2i position, int parent=-1)
         : m_parent(parent), m_position(position),m_g(0),m_h(0),m_f(0){
         }
         ~M_Node(){}
@@ -50,7 +54,7 @@ public:
     ~AStarPlanner();
     
     LocList run_astar();
-    M_Node* find( NodeList&, M_Node&);
+    NodeList::iterator find( NodeList&, M_Node&);
 
     friend bool operator==( AStarPlanner::M_Node&, AStarPlanner::M_Node&);
     friend bool operator==(const AStarPlanner::M_Node&, const AStarPlanner::M_Node&);
@@ -74,11 +78,11 @@ AStarPlanner::AStarPlanner(Eigen::MatrixXi &map,
 AStarPlanner::LocList AStarPlanner::run_astar(){
     // # Create start and end node
     // start_node = Node(None, start)
-    M_Node start_node(nullptr,m_start);
+    M_Node start_node(m_start);
     ROS_INFO_STREAM("start_node " << start_node.position()(0)<<","<<start_node.position()(1)); 
     // start_node.g = start_node.h = start_node.f = 0
     // end_node = Node(None, end)
-    M_Node end_node(nullptr,m_end);
+    M_Node end_node(m_end);
     if(start_node ==  end_node) return LocList();
 
     ROS_INFO_STREAM("end_node " << end_node.position()(0)<<","<<end_node.position()(1)); 
@@ -106,29 +110,34 @@ AStarPlanner::LocList AStarPlanner::run_astar(){
         // # Get the current node
         // current_node = open_list[0]
         M_Node current_node = open_list[0];
-        if( &current_node == current_node.parent() ){
-            ROS_ERROR("\n\n A* FAILED: &current_node == current_node.parent() \n\n");
-            ROS_ERROR_STREAM("parent: " << current_node.parent()->position()(0)<<","<<current_node.parent()->position()(1)); 
-            ROS_ERROR_STREAM("current_node: " << current_node.position()(0)<<","<<current_node.position()(1)); 
-            // abort();
-        }
         // current_index = 0
         int current_index = 0;
         // for index, item in enumerate(open_list):
+        ROS_INFO("Check open_list for lowest f-cost");
         for(int index=0; index<open_list.size(); index++)
         {
             M_Node item = open_list[index];
+            if(item.has_parent()) ROS_INFO_STREAM("   item "<<index<<" ("<<item.position()(0)<<","<<item.position()(1)<<")  p("<<closed_list[item.parent()].position()(0)<<","<<closed_list[item.parent()].position()(1)<<")  f:"<<item.f());
+            else ROS_INFO_STREAM("   item "<<index<<" ("<<item.position()(0)<<","<<item.position()(1)<<")  p(-,-)  f:"<<item.f());
             // if item.f < current_node.f:
             if (item.f() < current_node.f())
             {
                 // current_node = item
-                if(current_node ==  item && start_node == item) 
-                { ROS_INFO_STREAM(" #######your not adding yourself");  continue;}
+                // if(current_node ==  item && start_node == item) 
+                // { ROS_INFO_STREAM(" #######your not adding yourself");  continue;}
                 current_node = item;
                 // current_index = index
                 current_index = index;
             }
         }
+        ROS_INFO_STREAM("current_node: " << current_node.position()(0)<<","<<current_node.position()(1)); 
+        // if(current_node.parent()) ROS_INFO_STREAM("parent: " << current_node.parent()->position()(0)<<","<<current_node.parent()->position()(1)); 
+        // else ROS_INFO_STREAM("parent: -,-");
+
+        // if( &current_node == current_node.parent() ){
+        //     ROS_ERROR("\n\n A* FAILED: &current_node == current_node.parent() \n\n");
+        //     // abort();
+        // }
         ROS_INFO_STREAM("Moveing to closed_list.push_back(current_node:" << current_node.position()(0)<<","<<current_node.position()(1)); 
 
         // # Pop current off open list, add to closed list
@@ -143,33 +152,48 @@ AStarPlanner::LocList AStarPlanner::run_astar(){
 
         // closed_list.append(current_node)
         closed_list.push_back(current_node);
+        current_node.set_id(closed_list.size()-1);
+        // ROS_INFO("closed_list:");
+        // for(int i=0; i<closed_list.size(); i++)
+        // {
+        //     if(closed_list[i].parent()) ROS_INFO_STREAM("   item: ("<<closed_list[i].position()(0)<<","<<closed_list[i].position()(1)<<")  p("<<closed_list[i].parent()->position()(0)<<","<<closed_list[i].parent()->position()(0)<<")");
+        //     else ROS_INFO_STREAM("   item: ("<<closed_list[i].position()(0)<<","<<closed_list[i].position()(1)<<")  p(-,-)");
+        // }
+
+        // ROS_INFO_STREAM("2.current_node: " << current_node.position()(0)<<","<<current_node.position()(1)); 
+        // if(current_node.parent()) ROS_INFO_STREAM("2.parent: " << current_node.parent()->position()(0)<<","<<current_node.parent()->position()(1)); 
+        // else ROS_INFO_STREAM("2.parent: -,-");
 
         // # Found the goal
         // if current_node == end_node:
         if (current_node == end_node)
         {
             ROS_INFO_STREAM("Starting -> Found the goal");
+            ROS_INFO_STREAM("current_node: " << current_node.position()(0)<<","<<current_node.position()(1)); 
+            //ROS_INFO_STREAM("parent: " << current_node.parent()->position()(0)<<","<<current_node.parent()->position()(1)); 
+        
             // path = []
             LocList path;
             // current = current_node
-            M_Node *current = &current_node;
+            int current = current_node.id();
             // while current is not None:
-            while(current)
+            while(current>=0)
             {
-                ROS_INFO_STREAM("Found list current:" << current->position()(0)<<","<<current->position()(1)); 
-
+                // ROS_INFO_STREAM("Found list current:" << current->position()(0)<<","<<current->position()(1)); 
+                // ROS_INFO_STREAM("parent: " << current->parent()->position()(0)<<","<<current->parent()->position()(1)); 
+        
                 // path.append(current.position)
-                path.push_back(current->position());
+                path.push_back(closed_list[current].position());
                 // current = current.parent
-                if(current == current->parent())
-                {
-                    ROS_ERROR_STREAM("parent: " << current->parent()->position()(0)<<","<<current->parent()->position()(1)); 
-                    ROS_ERROR_STREAM("current_node: " << current->position()(0)<<","<<current->position()(1)); 
-                    ROS_ERROR("\n\nA* FAILED:  current == current->parent() \n\n");
-                    abort();
-                }
-                current = current->parent();
-                ROS_INFO_STREAM(" current = current->parent(); "); 
+                // if(current == current->parent())
+                // {
+                //     ROS_ERROR_STREAM("parent: " << current->parent()->position()(0)<<","<<current->parent()->position()(1)); 
+                //     ROS_ERROR_STREAM("current_node: " << current->position()(0)<<","<<current->position()(1)); 
+                //     ROS_ERROR("\n\nA* ERROR: ABORT:  current == current->parent() \n\n");
+                //     abort();
+                // }
+                current = closed_list[current].parent();
+                // ROS_INFO_STREAM(" current = current->parent(); "); 
             }
             // return path[::-1] # Return reversed path
             reverse(path.begin(),path.end());
@@ -179,7 +203,7 @@ AStarPlanner::LocList AStarPlanner::run_astar(){
 
         // # Generate children
         // children = []
-        NodeList children;
+        // NodeList children;
         // for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
         for( Vector2i new_position : LocList{Vector2i(0,-1),Vector2i(0,1),Vector2i(-1,0),Vector2i(1,0),
                                     Vector2i(-1,-1),Vector2i(-1,1),Vector2i(1,-1),Vector2i(1,1)})  //Adjacent squares
@@ -203,73 +227,79 @@ AStarPlanner::LocList AStarPlanner::run_astar(){
             // # Create new node
             // new_node = Node(current_node, node_position)
 
-            M_Node new_node = M_Node(&current_node, node_position);
+            if(current_node.has_parent()) ROS_INFO_STREAM("Create new node from parent("<<current_node.position()(0)<<","<<current_node.position()(1)<<")  p("<<closed_list[current_node.parent()].position()(0)<<","<<closed_list[current_node.parent()].position()(1)<<")");
+            else ROS_INFO_STREAM("Create new node from parent("<<current_node.position()(0)<<","<<current_node.position()(1)<<")  p(-,-)");
+            ROS_INFO_STREAM("   at position ("<<node_position(0)<<","<<node_position(1)<<")");
+            M_Node neighbor = M_Node(node_position,current_node.id());
 
             //Check if your not adding yourself
-            if(current_node ==  new_node && start_node == new_node) 
-            { ROS_INFO_STREAM("😅  Few dodged that one, your not adding yourself");  continue;}
+            // if(current_node ==  neighbor && start_node == neighbor) 
+            // { ROS_INFO_STREAM("😅  Few dodged that one, your not adding yourself");  continue;}
 
 
-            ROS_INFO_STREAM("parent: " << new_node.parent()->position()(0)<<","<<new_node.parent()->position()(1) ); 
-            ROS_INFO_STREAM("new_node: " << new_node.position()(0)<<","<<new_node.position()(1)); 
+            // ROS_INFO_STREAM("parent: " << neighbor.parent()->position()(0)<<","<<neighbor.parent()->position()(1) ); 
+            // ROS_INFO_STREAM("neighbor: " << neighbor.position()(0)<<","<<neighbor.position()(1)); 
 
-            if( &new_node == new_node.parent() ){
-                ROS_WARN("\n\n A* FAILED: &current_node == current_node.parent() \n\n");
-                ROS_WARN_STREAM("parent: " << new_node.parent()->position()(0)<<","<<new_node.parent()->position()(1)); 
-                ROS_WARN_STREAM("current_node: " << new_node.position()(0)<<","<<new_node.position()(1)); 
-                // abort();
-            }
+            // if( &neighbor == neighbor.parent() ){
+            //     ROS_WARN("\n\n A* FAILED: &current_node == current_node.parent() \n\n");
+            //     ROS_WARN_STREAM("parent: " << neighbor.parent()->position()(0)<<","<<neighbor.parent()->position()(1)); 
+            //     ROS_WARN_STREAM("current_node: " << neighbor.position()(0)<<","<<neighbor.position()(1)); 
+            //     // abort();
+            // }
  
 
 
-            // # Append
-            // children.append(new_node)
-            children.push_back(new_node);
-            ROS_INFO_STREAM(" children.append(new_node)"); 
-        }
+        //     // # Append
+        //     // children.append(neighbor)
+        //     children.push_back(neighbor);
+        //     ROS_INFO_STREAM(" children.append(neighbor)"<<neighbor.position()(0)<<","<<neighbor.position()(1)); 
+        // }
 
-        // # Loop through children
-        // for child in children:
-        for( M_Node child : children)
-        {
+        // // # Loop through children
+        // // for child in children:
+        // for( M_Node child : children)
+        // {
 
             // # Child is on the closed list
             // for closed_child in closed_list:
                 // if child == closed_child:
                     // continue
-            // NodeList::iterator it = find(closed_list.begin(),closed_list.end(),child);
-            // if (it != closed_list.end()) continue;
-            // M_Node closed_child;
-            ROS_INFO_STREAM("Checking if find(closed_list, child() " << child.position()(0)<<","<<child.position()(1)); 
+            ROS_INFO_STREAM("Checking if find neighbor " << neighbor.position()(0)<<","<<neighbor.position()(1)); 
 
-            if (find(closed_list, child)) continue;
+            // NodeList::iterator it = find(closed_list.begin(),closed_list.end(),neighbor);
+            NodeList::iterator it = find(closed_list,neighbor);
+            if (it != closed_list.end()) continue;
+            // M_Node closed_child;
+            // if (find(closed_list, child)) continue;
 
             // # Create the f, g, and h values
             // child.g = current_node.g + 1
-            child.set_g(current_node.g()+1);
+            neighbor.set_g(current_node.g()+1);
             // child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
-            child.set_h( (child.position() - end_node.position()).squaredNorm() );
+            neighbor.set_h( (neighbor.position() - end_node.position()).squaredNorm() );
             // child.f = child.g + child.h
-            child.set_f( child.g() + child.h() );
+            neighbor.set_f( neighbor.g() + neighbor.h() );
 
             // # Child is already in the open list
             // for open_node in open_list:
                 // if child == open_node and child.g > open_node.g:
                     // continue
-            // it = find(open_list.begin(),open_list.end(),child);
-            // if (it != open_list.end() && child.g() > it->g()) continue;
+            // it = find(open_list.begin(),open_list.end(),neighbor);
+            it = find(open_list,neighbor);
+            if (it != open_list.end() && neighbor.g() > it->g()) continue;
             // M_Node open_node = find(open_list,child);
-            M_Node *open_node = find(open_list,child);
-            if (open_node  && child.g() > open_node->g()) continue;
+            // M_Node *open_node = find(open_list,neighbor);
+            // if (open_node  && neighbor.g() > open_node->g()) continue;
 
             //Check if your not adding yourself
-            if(current_node ==  child && start_node == *open_node) 
-            { ROS_INFO_STREAM("😅  Few dodged that one, your not adding yourself");  continue;}
+            // if(current_node ==  neighbor && start_node == *open_node) 
+            // { ROS_INFO_STREAM("😅  Few dodged that one, your not adding yourself");  continue;}
 
             // # Add the child to the open list
             // open_list.append(child)
-            open_list.push_back(child);
-            ROS_INFO_STREAM("Add the \"child\" to the open_list ( " << child.position()(0)<<","<<child.position()(1) <<" )"); 
+            open_list.push_back(neighbor);
+            // M_Node it = open_list.back();
+            // ROS_INFO_STREAM("Add the \"neighbor\" to the open_list ( " << it.position()(0)<<","<<it.position()(1) <<" ); parent: "<<it.parent()->position()(0)<<","<<it.parent()->position()(1)); 
         }
     } 
 
@@ -285,28 +315,21 @@ AStarPlanner::~AStarPlanner()
 {
 }
 
-AStarPlanner::M_Node* AStarPlanner::find( NodeList& list, M_Node &node)
+AStarPlanner::NodeList::iterator AStarPlanner::find( NodeList& list, M_Node &node)
 {   
-    // for( M_Node m : list)
-    M_Node* m = &*(list.begin());
-    int i = 0;
     // ROS_INFO("find: while");
-    while(list.begin()+i != list.end())
+    for(NodeList::iterator m = list.begin(); m!=list.end(); m++)
     {
-        m = &*(list.begin()+i);
         // ROS_INFO_STREAM("  item: " << m->position()(0)<<","<<m->position()(1) ); 
         if (*m == node) {
-            if( m->parent() != node.parent()){
-                ROS_ERROR("\n m.parent() != node.parent()"); 
-                ROS_ERROR_STREAM("m " << m->position()(0)<<","<<m->position()(1)); 
-                // ROS_ERROR("node " << node.position()(0)<<","<<node.position()(1)); 
-
-
-            }
+            // if( m->parent() != node.parent()){
+            //     ROS_ERROR("\n m.parent() != node.parent()"); 
+            //     ROS_ERROR_STREAM("m " << m->position()(0)<<","<<m->position()(1)); 
+            //     // ROS_ERROR("node " << node.position()(0)<<","<<node.position()(1)); 
+            // }
 
             return m; 
         }
-        i++;
     }
-    return nullptr;
+    return list.end();
 }
